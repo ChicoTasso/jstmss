40476cff637337192fed6ad85a3e0ffe8c4ed802
ChicoTasso <tassomaisdogi@gmail.com>
2023-11-27 21:29:31 -0300

    caxa central

--- caxa/caxa.js
+++ caxa/caxa.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
     $('.box').click(function () {
+        $('.box').animate({width:'100px'});
+        $('.box').animate({height:'100px'});
+        $('.box').animate({width:'200px'});
+        $('.box').animate({height:'200px'});
         $('.box').animate({width:'300px'});
         $('.box').animate({height:'300px'});
-        $('.box').animate({marginLeft:'250px'});
         $('.box').animate({borderWidth:'20px'});
-        $('.box').animate({opacity:0.5});
+        $('.box').animate({opacity:1});
+        $('.box').animate({marginTop:'500px'});
+        $('.box').animate({marginBottom:'500px'});
+        $('.box').animate({marginLeft:'1100px'});
+        $('.box').animate({marginRight:'1100px'});
+        
     });
 })
