cf3fa5a5c9feac349ba054b4e314055a6b3ff947
ChicoTasso <tassomaisdogi@gmail.com>
2023-11-27 21:22:03 -0300

    animacao pumba

--- caxa/caxa.js
+++ caxa/caxa.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     $('.box').click(function () {
-        $('.box').animate({
-            width: '300px',
-            height:'300px',
-            marginLeft:'250px',
-            borderWidth:'20px',
-            opacity: 0.5,
-        }, 'slow');
+        $('.box').animate({width:'300px'});
+        $('.box').animate({height:'300px'});
+        $('.box').animate({marginLeft:'250px'});
+        $('.box').animate({borderWidth:'20px'});
+        $('.box').animate({opacity:0.5});
     });
-});
+})
