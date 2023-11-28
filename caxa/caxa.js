69010e0fa7d660346cf93cdc7bf1b36b1a4b92c0
ChicoTasso <tassomaisdogi@gmail.com>
2023-11-27 21:44:38 -0300

    implementei coisa massa

--- caxa/caxa.js
+++ caxa/caxa.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-    $('.box').click(function () {
-        $('.box').animate({width:'100px'});
-        $('.box').animate({height:'100px'});
-        $('.box').animate({width:'200px'});
-        $('.box').animate({height:'200px'});
-        $('.box').animate({width:'300px'});
-        $('.box').animate({height:'300px'});
-        $('.box').animate({borderWidth:'20px'});
-        $('.box').animate({opacity:1});
-        $('.box').animate({marginTop:'500px'});
-        $('.box').animate({marginBottom:'500px'});
-        $('.box').animate({marginLeft:'1100px'});
-        $('.box').animate({marginRight:'1100px'});
-        
+    $('#iniciar').click(function () {
+    $('.box').animate({
+        width:'300px',
+        height:'300px',
+        marginLeft:'250px',
+        borderWidth:'20px',
+        opacity:1,
+    },5000);
+    $('#parar').click(function () {
+        $('.box').stop ();
+    })  
     });
+    $('#reiniciar').click(function(){
+        $('.box').animate({
+            width: '100px',
+            height: '100px',
+            marginLeft:'250px',
+            borderWidth:'10px',
+            opacity:1
+
+        })
+    })
+    $('#sumir').click(function(){
+        $('.box').animate({
+            opacity:0,
+        })
+    })
+    $('#voltar').click(function(){
+        $('.box').animate({
+            opacity:1.0,
+        })
+    })
 })
